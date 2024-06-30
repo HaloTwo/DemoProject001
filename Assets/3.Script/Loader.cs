9368d08601121e2537951d789908ed412b7a9d7a
albusCyan <cyan@albusvr.com>
2024-06-30 18:24:35 +0900

    change

--- Assets/3.Script/Loader.cs
+++ Assets/3.Script/Loader.cs
@@ -9,6 +9,7 @@ public class Loader : MonoBehaviour
     [SerializeField] Image LoadImage;
     [SerializeField] float fadeTime = 1f;
     [SerializeField] float delayTime = 1f;
+
     private void Awake()
     {
         //데이터 로드
@@ -16,28 +17,7 @@ public class Loader : MonoBehaviour
 
     void Start()
     {
-
-        //FadeInOut_Dt();
         StartCoroutine(FadeInOut_Co());
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Start();
-        }
-    }
-
-
-
-
-    //Dotween 방식
-    void FadeInOut_Dt()
-    {
-        LoadImage.color = new Color(1, 1, 1, 0);
-
-        LoadImage.DOFade(1, fadeTime).SetDelay(delayTime).OnComplete(() => LoadImage.DOFade(0, fadeTime));
     }
 
 
@@ -45,7 +25,12 @@ public class Loader : MonoBehaviour
     IEnumerator FadeInOut_Co()
     {
         // 이미지의 알파값을 0으로 설정합니다.
-        LoadImage.color = new Color(1, 1, 1, 0);
+        Color orignColor = LoadImage.color;
+        LoadImage.color = new Color(orignColor.r, orignColor.g, orignColor.b, 0);
+
+        Text text = LoadImage.GetComponentInChildren<Text>();
+        Color textOrignColor = text.color;
+        text.color = new Color(textOrignColor.r, textOrignColor.g, textOrignColor.b, 0);
 
         // 지연 후 페이드 인 애니메이션 시작
         yield return new WaitForSeconds(delayTime);
@@ -56,8 +41,11 @@ public class Loader : MonoBehaviour
         {
             float alpha = Mathf.Lerp(0f, 1f, (Time.time - fadeTimer) / fadeTime);
             LoadImage.color = new Color(LoadImage.color.r, LoadImage.color.g, LoadImage.color.b, alpha);
+            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
             yield return null;
         }
+
+        yield return new WaitForSeconds(delayTime);
 
         // 페이드 아웃 애니메이션
         fadeTimer = Time.time;
@@ -65,10 +53,11 @@ public class Loader : MonoBehaviour
         {
             float alpha = Mathf.Lerp(1f, 0f, (Time.time - fadeTimer) / fadeTime);
             LoadImage.color = new Color(LoadImage.color.r, LoadImage.color.g, LoadImage.color.b, alpha);
+            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
             yield return null;
         }
 
 
-        SceneManager.LoadScene("Main",LoadSceneMode.Single);
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
     }
 }
--- Assets/3.Script/MainUI.cs
+++ Assets/3.Script/MainUI.cs
@@ -5,10 +5,16 @@ public class MainUI : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField]Rigidbody2D playerrid;
+    [SerializeField] Animator playeranim;
 
     void Start()
     {
-        
+        if (playerrid.TryGetComponent(out Animator anim))
+        {
+            playeranim = anim;
+        }
+
+
     }
 
     // Update is called once per frame
@@ -20,5 +26,15 @@ public class MainUI : MonoBehaviour
     public void JumpButton()
     {
         playerrid.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+    }
+
+    public void AttackButton()
+    {
+        playeranim.Play("attack");
+    }
+
+    public void HitButton()
+    {
+        playeranim.Play("hurt");
     }
 }
